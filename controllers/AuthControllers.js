15792fdfc01974f077145e2545b987cf1e6cae4e
Justin Junious <justinjunious@gmail.com>
2022-11-10 16:34:05 -0500

    added Login authentication

---
+++ controllers/AuthControllers.js
@@ -0,0 +1,29 @@
+const { User } = require('../models')
+const middleware = require('../middleware')
+
+const Login = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: { email: email.req.body },
+      raw: true
+    })
+    if (
+      user &&
+      (await middleware.comparePassword(user.passwordDigest, req.body.password))
+    ) {
+      let payload = {
+        id: user.id,
+        email: user.email
+      }
+      let token = middleware.createToken(payload)
+      return res.send({ user: payload, token })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized User' })
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  Login
+}
