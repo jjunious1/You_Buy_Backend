01e72272fdf490ab04f3dbfb9f8e797d4fb4e2a8
Christopher Yeom <chrisyeom97@gmail.com>
2022-11-10 12:02:34 -0500

    created a user seed and middleware

---
+++ seeders/20221110164007-user.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('users', [
+      {
+        name: 'Bob Smith',
+        email: 'bobsmith1@yahoo.com'
+      }
+    ])
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+}
