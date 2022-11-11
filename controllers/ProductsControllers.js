5ae13bb0ddc602d71e9a0062eb068c45a33c017c
Christopher Yeom <chrisyeom97@gmail.com>
2022-11-11 13:46:29 -0500

    tried to find comments by productid

--- controllers/ProductsControllers.js
+++ controllers/ProductsControllers.js
@@ -1,4 +1,4 @@
-const { Products, User } = require('../models')
+const { Products, User, Comment } = require('../models')
 
 const GetProducts = async (req, res) => {
   try {
@@ -11,6 +11,41 @@ const GetProducts = async (req, res) => {
   }
 }
 
+const GetProductsById = async (req, res) => {
+  try {
+    let productId = parseInt(req.params.id)
+    const comment = await Products.findByPk(productId, {
+      include: [
+        {
+          model: Comment,
+          as: 'review',
+          attributes: ['name', 'description'],
+          where: { id: productId }
+        }
+      ]
+    })
+    res.send(comment)
+  } catch (error) {
+    throw error
+  }
+}
+
+// const GetComments = async (req, res) => {
+//   try {
+//     let productId = parseInt(req.params.id)
+//     let comments = await Products.findAll({
+//       where: { id: productId },
+//       include: [
+//         { model: Comment, as: 'review', attributes: ['name', 'description'] }
+//       ]
+//     })
+//     res.send(comments)
+//   } catch (error) {
+//     throw error
+//   }
+// }
+
 module.exports = {
-  GetProducts
+  GetProducts,
+  GetProductsById
 }
