12d6736c2207d533117cb3285c607b43a64396da
Christopher Yeom <chrisyeom97@gmail.com>
2022-11-11 12:25:35 -0500

    created comment seed file

--- models/comment.js
+++ models/comment.js
@@ -9,7 +9,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Comment.belongsTo(models.Products, { foreignKey: 'productId' })
+      Comment.belongsTo(models.Products, {
+        as: 'review',
+        foreignKey: 'productId'
+      })
     }
   }
   Comment.init(
