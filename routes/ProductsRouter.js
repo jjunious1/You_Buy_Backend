596d4f9e895518d63460e872b94a9f5afa0fe5c7
Justin Junious <justinjunious@gmail.com>
2022-11-15 16:13:54 -0500

    updated products route

--- routes/ProductsRouter.js
+++ routes/ProductsRouter.js
@@ -1,6 +1,5 @@
 const router = require('express').Router()
 const controller = require('../controllers/ProductsControllers')
-const middleware = require('../middleware')
 
 router.get('/', controller.GetProducts)
 router.get('/:id', controller.GetCommentsInProduct)
