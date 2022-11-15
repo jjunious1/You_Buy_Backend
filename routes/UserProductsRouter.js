99e2148bda08b2695fa6ebff462d44066098605f
Justin Junious <justinjunious@gmail.com>
2022-11-15 14:16:27 -0500

    fixed routes

--- routes/UserProductsRouter.js
+++ routes/UserProductsRouter.js
@@ -2,7 +2,12 @@ const router = require('express').Router()
 const controller = require('../controllers/UserProductsControllers')
 const middleware = require('../middleware')
 
-router.get('/:id', controller.GetSales)
+router.get(
+  '/:id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.GetSales
+)
 router.post('/:id', controller.NewSell)
 router.put(
   '/:id',
