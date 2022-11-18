cdc00cb801d0e56828c5a1065c3b0ed73cdc5a08
Justin Junious <justinjunious@gmail.com>
2022-11-18 08:19:24 -0500

    commented out unused code and testing first

--- controllers/CommentsControllers.js
+++ controllers/CommentsControllers.js
@@ -13,53 +13,53 @@ const GetComments = async (req, res) => {
   }
 }
 
-const GetCommentsById = async (req, res) => {
-  try {
-    const comment = await Comment.findByPk(req.params.id)
-    res.send(comment)
-  } catch (error) {
-    throw error
-  }
-}
+// const GetCommentsById = async (req, res) => {
+//   try {
+//     const comment = await Comment.findByPk(req.params.id)
+//     res.send(comment)
+//   } catch (error) {
+//     throw error
+//   }
+// }
 
-const UpdateComment = async (req, res) => {
-  try {
-    let commentId = parseInt(req.params.id)
-    let updatedComment = await Comment.update(req.body, {
-      where: { id: commentId }
-    })
-    res.send(updatedComment)
-  } catch (error) {
-    throw error
-  }
-}
+// const UpdateComment = async (req, res) => {
+//   try {
+//     let commentId = parseInt(req.params.id)
+//     let updatedComment = await Comment.update(req.body, {
+//       where: { id: commentId }
+//     })
+//     res.send(updatedComment)
+//   } catch (error) {
+//     throw error
+//   }
+// }
 
-const CreateComments = async (req, res) => {
-  try {
-    let commentBody = {
-      ...req.body
-    }
-    let comment = await Comment.create(commentBody)
-    res.send(comment)
-  } catch (error) {
-    throw error
-  }
-}
+// const CreateComments = async (req, res) => {
+//   try {
+//     let commentBody = {
+//       ...req.body
+//     }
+//     let comment = await Comment.create(commentBody)
+//     res.send(comment)
+//   } catch (error) {
+//     throw error
+//   }
+// }
 
-const DeleteComment = async (req, res) => {
-  try {
-    let commentId = parseInt(req.params.id)
-    await Comment.destroy({ where: { id: commentId } })
-    res.send({ message: `Deleted comment with an id of ${commentId}` })
-  } catch (error) {
-    throw error
-  }
-}
+// const DeleteComment = async (req, res) => {
+//   try {
+//     let commentId = parseInt(req.params.id)
+//     await Comment.destroy({ where: { id: commentId } })
+//     res.send({ message: `Deleted comment with an id of ${commentId}` })
+//   } catch (error) {
+//     throw error
+//   }
+// }
 
 module.exports = {
-  GetComments,
-  GetCommentsById,
-  UpdateComment,
-  CreateComments,
-  DeleteComment
+  GetComments
+  // GetCommentsById,
+  // UpdateComment,
+  // CreateComments,
+  // DeleteComment
 }
--- routes/CommentsRouter.js
+++ routes/CommentsRouter.js
@@ -1,10 +1,9 @@
 const router = require('express').Router()
 const controller = require('../controllers/CommentsControllers')
-const middleware = require('../middleware')
 
 router.get('/', controller.GetComments)
-router.get('/:id', controller.GetCommentsById)
-router.put('/:id', controller.UpdateComment)
-router.post('/:id', controller.CreateComments)
-router.delete('/:id', controller.DeleteComment)
+// router.get('/:id', controller.GetCommentsById)
+// router.put('/:id', controller.UpdateComment)
+// router.post('/:id', controller.CreateComments)
+// router.delete('/:id', controller.DeleteComment)
 module.exports = router
